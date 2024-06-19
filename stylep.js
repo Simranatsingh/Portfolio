68499acf73b554200889f3f32bf5735baf7248a7
Simran Singh <simranatsingh7j@gmail.com>
2024-06-19 18:02:39 +0530

    Adding portfolio files
    no readme till now

---
+++ stylep.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.querySelector(".main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.querySelector(".main").style.marginLeft = "0";
+}
